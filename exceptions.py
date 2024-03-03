4736a8ce54eaacb0b14157799cdd4f9e8a01852e
barbarosa <tandogankaan5@gmail.com>
2024-03-03 14:38:35 +0300

    14.38

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,13 @@
-def Func1(var):
+def func1(var):
     try:
+        var = int(var)
         return 10/var
-    except Exception as e:
+    except ZeroDivisionError:  # You can specify a certain error and give an error message for that manually.
+        print('You can\'t enter zero ')
+    except Exception as e:  # Or you can print the default exception
         print(e)
 
 
-a = Func1(0)
-
-
-
-
+if __name__ == '_main_':
+    val = input("Enter something: ")
+    a = func1(val)
