8a455ba3bafb458feaffa57e0369d89b2f3e1fc2
barbarosa <tandogankaan5@gmail.com>
2024-07-18 23:58:42 +0300

    end of day commits

---
+++ lists.py
@@ -0,0 +1,19 @@
+spam = ['cat', 'bat', 'rat', 'elephant']
+print("spam[0] is: ",spam[0])
+print("spam[-1] is: ",spam[-1])
+print("spam[1:3] is: ",spam[1:3])
+print("Before deleting with del spam[2], spam is",spam)
+del spam[2]
+print("After spam is: ",spam)
+
+str_sum = "Hello" + " World"
+print(str_sum)
+
+print("Hello "*3)
+print([1,2,3,'a']*3)
+
+print(type(int('42')))
+print(list('Hello'))
+
+print('theta' in ['alpha','beta','theta','omega'])
+print('zeta' in ['alpha','beta','theta','omega'])
