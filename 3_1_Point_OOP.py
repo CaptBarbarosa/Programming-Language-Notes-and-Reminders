3430c3359a58e07355b7ef55326a05d5c3347a3e
barbarosa <tandogankaan5@gmail.com>
2024-07-28 13:36:27 +0300

    OOP Point changes are done

--- 3_1_Point_OOP.py
+++ 3_1_Point_OOP.py
@@ -14,14 +14,18 @@ class Employee:
     @staticmethod
     def is_crowded():
         return Employee.number_of_employees > Employee.max_employees
-
+    
     def __init__(self, f_name, l_name):
         self.f_name = f_name
         self.l_name = l_name
         Employee.number_of_employees += 1
+    def __str__(self):
+        return "hello"
 
 
 if __name__ == "__main__":
     c = Point()
     c.__x = 10  # It doesn't affect the private variable.
     c.print_point()
+    emp = Employee("John", "Doe")
+    print(emp)
