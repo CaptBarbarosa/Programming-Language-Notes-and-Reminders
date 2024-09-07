2e973422f02ab4ddcee5607c4b1b21044caf9e31
barbarosa <tandogankaan5@gmail.com>
2024-09-07 23:53:23 +0300

    end of the day

---
+++ angela_yu_python_lectures/Day 25/main.py
@@ -0,0 +1,11 @@
+import csv
+temperatures = []
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    for row in data:
+        print(row)
+        temperatures.append(row[1])
+temperatures.pop(0)
+print(temperatures)
+
