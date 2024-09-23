677c385d4cd9b794a813bed320fa052f284006de
barbarosa <tandogankaan5@gmail.com>
2024-09-23 18:35:14 +0300

    kwargs added

--- angela_yu_python_lectures/Day 27/2_unlimited_number_of_inputs.py
+++ angela_yu_python_lectures/Day 27/2_unlimited_number_of_inputs.py
@@ -4,5 +4,14 @@ def add(*args):
         sum += n
     return sum
 
+def calculate(**kwargs): #We can also use kwargs in class creation.
+    print(type(kwargs))
+    print(kwargs)
+    for key, value in kwargs.items():
+        print("key: ",key, " value: ",value)
+    
+
 if __name__ == "__main__":
     print(add(1,2,3,6,4,5))
+    calculate(add=3, multiply=5)
+    
