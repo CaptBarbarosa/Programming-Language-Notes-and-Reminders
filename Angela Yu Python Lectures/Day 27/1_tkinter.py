24689c96c32319d6bc0729453f13bfc39d9b1d45
barbarosa <tandogankaan5@gmail.com>
2024-09-23 18:53:41 +0300

    End of the day

--- Angela Yu Python Lectures/Day 27/1_tkinter.py
+++ Angela Yu Python Lectures/Day 27/1_tkinter.py
@@ -9,6 +9,12 @@ def create_window():
 
     my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold")) # Created the label
     my_label.pack( ) #Shows the label.
+    
+    # Let's say you want to change the text, or foreground or background you can do:
+    #my_label["text"] = "New Text"
+    my_label.config(text="New Text")
+    
+    #button = tkinter.Button(text=)
 
 
     window.mainloop() # This will keep the window running.
