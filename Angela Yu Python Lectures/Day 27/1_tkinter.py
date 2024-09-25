0c4032957546fba4d0f499f25aee96ed0140f6e7
barbarosa <tandogankaan5@gmail.com>
2024-09-26 00:08:18 +0300

    end of day

--- Angela Yu Python Lectures/Day 27/1_tkinter.py
+++ Angela Yu Python Lectures/Day 27/1_tkinter.py
@@ -8,22 +8,28 @@ def create_window():
     window.minsize(width=500, height=300)
 
     my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold")) # Created the label
-    my_label.pack() #Shows the label.
+    #my_label.pack() #Shows the label.
+    my_label.grid(column=0, row=0)
     
     # Let's say you want to change the text, or foreground or background you can do:
     #my_label["text"] = "New Text"
     #my_label.place(x=100, y=0)
-    my_label.config(text="New Text")
-    
     
     input = tkinter.Entry(width=15)
-    input.pack()
+    #input.pack()
+    input.grid(column=3, row=2)
     
     #IMPORTANT
     #Lambda allows you to create an anonymous function that will defer execution of button_clicked until the button is clicked.
     #Without lambda it will automatically execute the function.
     button = tkinter.Button(text="Click Here", command=lambda: button_clicked(my_label, input))
-    button.pack()
+    #button.pack()
+    button.grid(column=1, row=1)
+    
+            #You can't have grid and pack together.
+    
+    button2 = tkinter.Button(text="Button2")
+    button2.grid(column=2, row=0)
 
 
     window.mainloop() # This will keep the window running.
