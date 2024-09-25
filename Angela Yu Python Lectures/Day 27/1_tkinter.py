47d98d7991bb20c1d413cf4888d96c20fb8f350b
barbarosa <tandogankaan5@gmail.com>
2024-09-25 23:56:07 +0300

    updates on GUI

--- Angela Yu Python Lectures/Day 27/1_tkinter.py
+++ Angela Yu Python Lectures/Day 27/1_tkinter.py
@@ -8,16 +8,16 @@ def create_window():
     window.minsize(width=500, height=300)
 
     my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold")) # Created the label
-    my_label.pack( ) #Shows the label.
+    my_label.pack() #Shows the label.
     
     # Let's say you want to change the text, or foreground or background you can do:
     #my_label["text"] = "New Text"
+    #my_label.place(x=100, y=0)
     my_label.config(text="New Text")
     
     
     input = tkinter.Entry(width=15)
     input.pack()
-    
     
     #IMPORTANT
     #Lambda allows you to create an anonymous function that will defer execution of button_clicked until the button is clicked.
@@ -28,16 +28,16 @@ def create_window():
 
     window.mainloop() # This will keep the window running.
 
+
 def button_clicked(my_label, input):
-    
     #This will change the text of the window when button clicked.
     """
     my_label.config(text="You clicked the button")
     print("Button is clicked")
     """
-    
     #This will change the label to what the user enters as an entry.
     my_label.config(text=input.get())
+
 
 if __name__ == "__main__":
     create_window()
