5146c28ae2686f88552f347228ba3316534f448a
barbarosa <tandogankaan5@gmail.com>
2024-10-20 00:19:36 +0300

    End of the day

--- Java/Fifth_Input_Output_Streams/src/Main.java
+++ Java/Fifth_Input_Output_Streams/src/Main.java
@@ -1,4 +1,18 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        File file = new File("src/examples/0_test");
+        System.out.println("Does it exist? " + file.exists());
+        System.out.println("The file has " + file.length() + " bytes");
+        System.out.println("Can it be read? " + file.canRead());
+        System.out.println("Can it be written? " + file.canWrite());
+        System.out.println("Is it a directory? " + file.isDirectory());
+        System.out.println("Is it a file? " + file.isFile());
+        System.out.println("Is it absolute? " + file.isAbsolute());
+        System.out.println("Is it hidden? " + file.isHidden());
+        System.out.println("Absolute path is " + file.getAbsolutePath());
+        System.out.println("Last modified on " + new java.util.Date(file.lastModified()));
+        System.out.println("Seperator:"+java.io.File.separator);
     }
 }
