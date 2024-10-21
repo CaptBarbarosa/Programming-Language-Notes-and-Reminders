4ab26e02aa0d5cbb8c4146fe856575ecaba8308c
barbarosa <tandogankaan5@gmail.com>
2024-10-21 13:45:49 +0300

    updates

--- Java/Fifth_Input_Output_Streams/src/First_File_Streams.java
+++ Java/Fifth_Input_Output_Streams/src/First_File_Streams.java
@@ -1,5 +1,19 @@
-public class First_File_Streams {
-    public static void main(String[]args){
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class First_File_Streams {
+    public static void main(String[]args) throws IOException {
+        FileOutputStream output = new FileOutputStream("src/examples/First_example.dat");
+        for(int i=1; i <= 10; i++){
+            output.write(i);}
+        output.close();
+
+        FileInputStream input = new FileInputStream("src/examples/First_example.dat");
+        int val;
+        while((val = input.read())!=-1)
+            System.out.println("Value is: "+val);
+        input.close();
     }
 }
