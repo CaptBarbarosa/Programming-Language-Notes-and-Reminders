2ca1c8d75f1b536b8145631957f33a9a0ea853e0
barbarosa <tandogankaan5@gmail.com>
2024-12-12 16:10:34 +0300

    Csharp added

--- C# BTK Akademi/First Project/CSharpCourse/ConsoleApp1/Program.cs
+++ C# BTK Akademi/First Project/CSharpCourse/ConsoleApp1/Program.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args){
-            Console.WriteLine("Hello again C#");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
