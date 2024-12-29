a8702a1986a6c3cd4d3c4a02022aeb9a2babda99
barbarosa <tandogankaan5@gmail.com>
2024-12-29 20:08:27 +0300

    Strings in C# added

---
+++ C# BTK Akademi/Strings_in_CSharp/Strings_in_CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_in_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "Ankara", city2 = "Istanbul";
+            Console.WriteLine(String.Format("{0} {1}",city, city2)); //If we define this within the Console.WriteLine we didn't create a new variable
+        }
+    }
+}
