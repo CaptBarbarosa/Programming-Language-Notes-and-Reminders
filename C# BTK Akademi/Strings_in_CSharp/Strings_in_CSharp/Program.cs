f355c7602bd5647a757a23b2d92f24b243dae450
barbarosa <tandogankaan5@gmail.com>
2024-12-29 21:49:19 +0300

    strings are done

--- C# BTK Akademi/Strings_in_CSharp/Strings_in_CSharp/Program.cs
+++ C# BTK Akademi/Strings_in_CSharp/Strings_in_CSharp/Program.cs
@@ -12,6 +12,20 @@ namespace Strings_in_CSharp
         {
             string city = "Ankara", city2 = "Istanbul";
             Console.WriteLine(String.Format("{0} {1}",city, city2)); //If we define this within the Console.WriteLine we didn't create a new variable
+            int res = city.Length;
+            Console.WriteLine(res);
+            var city3 = city.Clone();
+            Console.WriteLine(city3);
+
+            bool ends_with = city.EndsWith("a");
+            Console.WriteLine(ends_with);
+
+            string name = "Hi, my name is Kaan";
+
+            var index_of = name.IndexOf("name"); // If it is unable to find it will return -1.
+            Console.WriteLine(index_of);
+
+
         }
     }
 }
