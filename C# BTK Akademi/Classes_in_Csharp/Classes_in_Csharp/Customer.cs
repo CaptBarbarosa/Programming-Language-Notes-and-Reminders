e4c55327951489ec7fed2af114ae1648480e9af9
barbarosa <tandogankaan5@gmail.com>
2024-12-31 01:48:29 +0300

    end of the day commits

---
+++ C# BTK Akademi/Classes_in_Csharp/Classes_in_Csharp/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_in_Csharp
+{
+    internal class Customer
+    {
+        // Properties
+        /*
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        */
+        private int _id;
+        public int Id 
+        {
+            get { return _id; } 
+            set { _id = value; }
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get {
+                Console.WriteLine("You are in the get method for _firstName");
+                return _firstName; 
+            }
+            set {
+                Console.WriteLine("You are in the set method for _firstName");
+                _firstName = value; 
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        private string _address;
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+    }
+}
--- C# BTK Akademi/Classes_in_Csharp/Classes_in_Csharp/Program.cs
+++ C# BTK Akademi/Classes_in_Csharp/Classes_in_Csharp/Program.cs
@@ -10,6 +10,22 @@ namespace Classes_in_Csharp
     {
         static void Main(string[] args)
         {
+            CustomerManager myCustomerManager = new CustomerManager();
+            myCustomerManager.Add();
+            myCustomerManager.Update();
+
+            Customer my_customer = new Customer();
+            my_customer.Id=1;
+            my_customer.FirstName = "Kaan";
+
+            Customer my_customer2 = new Customer{Id = 2, FirstName = "Kaan", 
+                LastName = "2", Address = "Istanbul"
+            };
+
+            Console.WriteLine(my_customer2.FirstName);
+            
         }
     }
+
+    
 }
